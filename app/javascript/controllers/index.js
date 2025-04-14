e1c9c9adb9dc41b1e35c6113a1defe5cb411183b
Joshua Flark <joshuajmark@gmail.com>
2025-04-13 19:26:38 -0700

    Install rubyui dropdown

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import EditorController from "./editor_controller"
 application.register("editor", EditorController)
+
+import RubyUi__DropdownMenuController from "./ruby_ui/dropdown_menu_controller"
+application.register("ruby-ui--dropdown-menu", RubyUi__DropdownMenuController)
