036e476f57e613d346ab2b29b60f25e6da855949
Joshua Flark <joshuajmark@gmail.com>
2025-05-06 16:29:17 -0700

    Refactor initializer

--- config/initializers/ruby_ui.rb
+++ config/initializers/ruby_ui.rb
@@ -17,26 +17,19 @@ Rails.autoloaders.main.inflector.inflect(
   "ruby_ui" => "RubyUI"
 )
 
-# Allow using RubyUI::ComponentName instead Components::RubyUI::ComponentName
 Rails.autoloaders.main.push_dir(
   Rails.root.join("app/components/ruby_ui"), namespace: RubyUI
 )
 
-# Allow using RubyUI::ComponentName instead RubyUI::ComponentName::ComponentName
-Rails.autoloaders.main.collapse(Rails.root.join("app/components/ruby_ui/*"))
-
-# Add Tiptap phlex components
 Rails.autoloaders.main.push_dir(
   Rails.root.join("app/components/tiptap"), namespace: Tiptap
 )
 
-# Allow using Tiptap::ComponentName instead Tiptap::ComponentName::ComponentName
-Rails.autoloaders.main.collapse(Rails.root.join("app/components/tiptap/*"))
-
-# Document phlex components
 Rails.autoloaders.main.push_dir(
   Rails.root.join("app/components/documents"), namespace: Documents
 )
 
-# Allow using Document::ComponentName instead Document::ComponentName::ComponentName
+# Allow using Namespace::ComponentName instead Components::Namespace::ComponentName
+Rails.autoloaders.main.collapse(Rails.root.join("app/components/ruby_ui/*"))
 Rails.autoloaders.main.collapse(Rails.root.join("app/components/documents/*"))
+Rails.autoloaders.main.collapse(Rails.root.join("app/components/tiptap/*"))
